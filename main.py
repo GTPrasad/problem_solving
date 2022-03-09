c9e96bf2cd84bc81fa35721394a6bce4c166e195
Prasad G T <goud@adobe.com>
2022-03-09 09:26:31 +0530

    updating the file name

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
-import permutaion
+import permutation
 
 s = 'ABCDD'
-permutaion.permutations(list(s))
-permutaion.using_iter(s)
+permutation.permutations(list(s))
+permutation.using_iter(s)
 
 if __name__ == '__main__':
     print("File one __name__ is set to: {}".format(__name__))
